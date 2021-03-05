fe000e3b33f9d6757d225490c4c8ec9dd81c7924
Ashutosh Panda <ashutosh.pand@mail.vinove.com>
2021-03-05 19:29:35 +0530

    User Card

--- random-user/src/App.vue
+++ random-user/src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="app p-4 conatiner-fluid">
+      <div class="row">
+        <div class="col col-md-4 mt-4 offset-md-4">
+          <div class="text-center">
+            <div v-if="user">
+              <UserCard :user="user"></UserCard>
+              <a
+                class="btn btn-warning btn-block mt-3 btn-lg"
+                v-on:click="fetchUser"
+              >
+                Cick To Fetch</a
+              >
+            </div>
+            <div v-else class="text-center">
+              <div class="spinner-border spinner-grow" role="status">
+                <span class="sr-onl">Loading...</span>
+              </div>
+              <div class="text-info">Loading...</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <a class="btn btn-primary" v-on:click="fetchUser"> cick to fetch</a> -->
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import UserCard from "./components/UserCard/UserCard.vue";
 
 export default {
-  name: 'App',
+  name: "Random User App",
+  data() {
+    return {
+      user: null,
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    UserCard,
+  },
+
+  mounted() {
+    this.fetchUser();
+  },
+
+  methods: {
+    fetchUser: async function () {
+      try {
+        const {
+          data: { results },
+        } = await axios.get(`https://randomuser.me/api`);
+        this.user = results[0];
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+main {
+  height: 100vh;
+  background-image: url("./assets/lco-api-one.jpg");
+  background-position: center;
+  background-size: cover;
+}
+
+.card {
+  margin-top: 125px;
+}
+
+.img {
+  margin-top: -80px;
 }
 </style>
---
+++ random-user/src/components/UserCard/UserCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card border-primary">
+    <div class="card-body text-center">
+      <img
+        v-bind:src="user?.picture?.large"
+        alt="User Avatar"
+        height="150"
+        width="150"
+        class="rounded-circle img-thumbnail zoomIn"
+      />
+
+      <div class="card-title text-primary">
+        <h2 class="my-3">
+          <span class="pl-2">{{ user?.name?.title }}</span>
+          <span class="pl-2">{{ user?.name?.first }}</span>
+          <span class="pl-2">{{ user?.name?.last }}</span>
+        </h2>
+        <p class="badge bg-success text-white">
+          {{ user?.gender }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Uer Card",
+  props: ["user"],
+};
+</script>
+
+<style>
+</style>
